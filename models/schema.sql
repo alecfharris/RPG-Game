74e32cff726cedd03ab9307b35f38848b731e3ec
Alec Harris <alecfharris@gmail.com>
2018-12-05 14:33:47 -0600

    added attacks get request

--- models/schema.sql
+++ models/schema.sql
@@ -4,6 +4,8 @@ CREATE DATABASE exampledb;
 DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb;
 
+DROP DATABASE IF EXISTS rpg_game;
+CREATE DATABASE rpg_game;
 USE rpg_game;
 
 CREATE TABLE attacks
@@ -13,6 +15,6 @@ id int NOT NULL AUTO_INCREMENT,
 Weapon varchar(11) NOT NULL,
 Power int NOT NULL,
 Accuracy int NOT NULL,
-Physical_Magical tinyint NOT NULL,
+Magical tinyint NOT NULL,
 PRIMARY KEY (id)
 );
