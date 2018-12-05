9954d2f6fb808494c23112d7504f899e912f493c
Alec Harris <alecfharris@gmail.com>
2018-12-05 11:42:43 -0600

    updated seeds and schema

--- models/schema.sql
+++ models/schema.sql
@@ -3,3 +3,16 @@ CREATE DATABASE exampledb;
 
 DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb;
+
+USE rpg_game;
+
+CREATE TABLE attacks
+
+(
+id int NOT NULL AUTO_INCREMENT,
+Weapon varchar(11) NOT NULL,
+Power int NOT NULL,
+Accuracy int NOT NULL,
+Physical_Magical tinyint NOT NULL,
+PRIMARY KEY (id)
+);
---
+++ models/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO attacks (Weapon, Power, Accuracy, Physical_Magical) VALUES (Bow, 60, 100, False);
+INSERT INTO attacks (Weapon, Power, Accuracy, Physical_Magical) VALUES (Sword, 90, 95, False);
+INSERT INTO attacks (Weapon, Power, Accuracy, Physical_Magical) VALUES (Axe, 110, 80, False);
