3236dc6d2319fa1501a33cf4d855240b5e2217d1
Rebecca Baker <rebecca122@mac.com>
2018-12-05 20:29:52 -0600

    added character stats

--- models/schema.sql
+++ models/schema.sql
@@ -19,9 +19,14 @@ Magical tinyint NOT NULL,
 PRIMARY KEY (id)
 );
 
+INSERT INTO attacks (Weapon, Power, Accuracy, Magical) VALUES ("Bow", 60, 100, False);
+INSERT INTO attacks (Weapon, Power, Accuracy, Magical) VALUES ("Sword", 90, 95, False);
+INSERT INTO attacks (Weapon, Power, Accuracy, Magical) VALUES ("Axe", 110, 80, False);
+
+
 CREATE TABLE characters (
     id int NOT NULL AUTO_INCREMENT,
-    Name varchar(11) NOT NULL,
+    Name varchar(20) NOT NULL,
     Level int NOT NULL,
     HP int NOT NULL,
     Physical_Attack int NOT NULL,
@@ -30,6 +35,20 @@ CREATE TABLE characters (
     Magical_Defense int NOT NULL,
     Speed int NOT NULL,
     Weapons tinyint NOT NULL,
-    Mage_Magic tinyint NOT NULL
+    Mage_Magic tinyint NOT NULL,
     PRIMARY KEY (id)
 );
+
+INSERT INTO characters (Name, Level, HP, Physical_Attack, Physical_Defense, Magical_Attack, Magical_Defense, Speed, Weapons, Mage_Magic) VALUES ("Jacob", 50, 133, 50, 65, 100, 100, 75, False, True);
+
+INSERT INTO characters (Name, Level, HP, Physical_Attack, Physical_Defense, Magical_Attack, Magical_Defense, Speed, Weapons, Mage_Magic) VALUES ("LJ", 50, 135, 100, 75, 85, 75, 100, True, True);
+
+INSERT INTO characters (Name, Level, HP, Physical_Attack, Physical_Defense, Magical_Attack, Magical_Defense, Speed, Weapons, Mage_Magic) VALUES ("Nick", 50, 160, 80, 100, 80, 100, 65, True, False);
+
+INSERT INTO characters (Name, Level, HP, Physical_Attack, Physical_Defense, Magical_Attack, Magical_Defense, Speed, Weapons, Mage_Magic) VALUES ("Enemy Spellcaster", 55, 167, 107, 107, 141, 141, 105, False, True);
+
+INSERT INTO characters (Name, Level, HP, Physical_Attack, Physical_Defense, Magical_Attack, Magical_Defense, Speed, Weapons, Mage_Magic) VALUES ("Enemy FightMage", 55, 170, 132, 132, 113, 113, 140, True, True);
+
+INSERT INTO characters (Name, Level, HP, Physical_Attack, Physical_Defense, Magical_Attack, Magical_Defense, Speed, Weapons, Mage_Magic) VALUES ("Enemy Paladin", 55, 190, 113, 145, 113, 145, 95, True, False);
+
+INSERT INTO characters (Name, Level, HP, Physical_Attack, Physical_Defense, Magical_Attack, Magical_Defense, Speed, Weapons, Mage_Magic) VALUES ("Ultimate Enemy", 60, 232, 167, 167, 167, 167, 167, True, True);
