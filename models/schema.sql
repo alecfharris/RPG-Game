36bf9d0186ca53b7cc308e77c2b0dcafead7870d
Alec Harris <alecfharris@gmail.com>
2018-12-05 19:28:58 -0600

    created table

--- models/schema.sql
+++ models/schema.sql
@@ -18,3 +18,18 @@ Accuracy int NOT NULL,
 Magical tinyint NOT NULL,
 PRIMARY KEY (id)
 );
+
+CREATE TABLE characters (
+    id int NOT NULL AUTO_INCREMENT,
+    Name varchar(11) NOT NULL,
+    Level int NOT NULL,
+    HP int NOT NULL,
+    Physical_Attack int NOT NULL,
+    Physical_Defense int NOT NULL,
+    Magical_Attack int NOT NULL,
+    Magical_Defense int NOT NULL,
+    Speed int NOT NULL,
+    Weapons tinyint NOT NULL,
+    Mage_Magic tinyint NOT NULL
+    PRIMARY KEY (id)
+);
